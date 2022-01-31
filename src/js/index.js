15a5864487ec9303214bb2139ad525242c03b020
Christiano0407 <placebo0704@gmail.com>
2022-01-31 10:57:58 -0600

    add carousel-slide

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,27 @@
 // == Swipper ==
-const swiper = new Swiper(".swiper", {
-  direction: "horizontal",
-  loop: true,
-
-  /* pagination: { */
-  /*   el: ".swiper-pagination", */
-  /* }, */
-
-  navigation: {
-    nextEl: `.swiper-button-next`,
-    prevEl: `.swiper-button-prev`,
-  },
-
-  scrollbar: {
-    el: `.swiper-scrollbar`,
+/* const swiper = new Swiper(".swiper", { */
+/*   direction: "horizontal", */
+/*   loop: true, */
+/*  */
+/*   pagination: { */
+/*     el: ".swiper-pagination", */
+/*   }, */
+/*  */
+/*   navigation: { */
+/*     nextEl: `.swiper-button-next`, */
+/*     prevEl: `.swiper-button-prev`, */
+/*   }, */
+/*  */
+/*   scrollbar: { */
+/*     el: `.swiper-scrollbar`, */
+/*   }, */
+/* }); */
+/*  = new =*/
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
   },
 });
